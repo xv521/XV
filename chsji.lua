e9bccadc9caa3f6a26cff7e4e31fc7e5af00a9d9
江湖有酒 <74492729+xv521@users.noreply.github.com>
2022-07-24 17:03:59 +0800

    Update chsji.lua

--- chsji.lua
+++ chsji.lua
@@ -1,3 +1,7 @@
-- headers:
-    User-Agent: 'baiduboxapp: rule'
-    X-T5-Auth: '1962898709'
+-- file: lua/backend-baidu.lua
+
+        local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
+                    'Host: ' .. host .. ':' .. port .. '\r\n' ..
+                    'Proxy-Connection: Keep-Alive\r\n'..
+                    'X-T5-Auth: YTY0Nzlk\r\n' ..
+                    'User-Agent: baiduboxapp\r\n\r\n'
